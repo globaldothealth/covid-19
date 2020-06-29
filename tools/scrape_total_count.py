e65893ecc73f3b8b30f3b190094c9648aebaba30
Manu Cornet <m@ma.nu>
2020-06-29 19:59:54 +0000

    Also count total number of deaths

--- tools/scrape_total_count.py
+++ tools/scrape_total_count.py
@@ -6,16 +6,17 @@ import pandas as pd
 # Returns whether the operation was successful
 def scrape_total_count(out_path):
     url = "http://opendata.ecdc.europa.eu/covid19/casedistribution/csv/"
-    try: 
-        data = pd.read_csv(url, usecols=['cases'])
+    try:
+        data = pd.read_csv(url, usecols=['cases', 'deaths'])
     except:
         print("I wasn't able to fetch the file I wanted: " + str(url))
         return False
 
     count = data['cases'].sum()
+    deaths = data['deaths'].sum()
 
     date = datetime.now().strftime("%Y-%m-%d")
-    results = {"caseCount": int(count), "date": date}
+    results = {"caseCount": int(count), "deaths": int(deaths), "date": date}
 
     with open(out_path, "w") as F:
         json.dump([results], F)
